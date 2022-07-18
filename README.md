0f65a93b97ea896a71d3eee32c109e50d8e6fe74
I am Lu <contact@iamlu.net>
2022-07-17 22:20:22 -0600

    Initial load

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# python-sharepoint-office365-api
+# Python SharePoint Office365 API
+You will find example on connecting to Office 365 SharePoint using the Office 365 Rest Python Client package.
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`sharepoint_email`
+
+`sharepoint_password`
+
+`sharepoint_url_site`
+
+`sharepoint_site_name`
+
+`sharepoint_doc_library`
+
---
+++ office365_api.py
@@ -0,0 +1,36 @@
+import environ
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.user_credential import UserCredential
+from office365.sharepoint.files.file import File
+
+env = environ.Env()
+environ.Env.read_env()
+
+USERNAME = env('sharepoint_email')
+PASSWORD = env('sharepoint_password')
+SHAREPOINT_SITE = env('sharepoint_url_site')
+SHAREPOINT_SITE_NAME = env('sharepoint_site_name')
+SHAREPOINT_DOC = env('sharepoint_doc_library')
+
+class SharePoint:
+    def _auth(self):
+        conn = ClientContext(SHAREPOINT_SITE).with_credentials(
+            UserCredential(
+                USERNAME,
+                PASSWORD
+            )
+        )
+        return conn
+
+    def _get_files_list(self, folder_name):
+        conn = self._auth()
+        target_folder_url = f'{SHAREPOINT_DOC}/{folder_name}'
+        root_folder = conn.web.get_folder_by_server_relative_url(target_folder_url)
+        root_folder.expand(["Files", "Folders"]).get().execute_query()
+        return root_folder.files
+
+    def download_file(self, file_name, folder_name):
+        conn = self._auth()
+        file_url = f'/sites/{SHAREPOINT_SITE_NAME}/{SHAREPOINT_DOC}/{folder_name}/{file_name}'
+        file = File.open_binary(conn, file_url)
+        return file.content
